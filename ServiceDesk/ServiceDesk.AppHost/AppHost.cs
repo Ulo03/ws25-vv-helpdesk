b116fd31f5875aac241b3a86fea5ecd1e2b80d6c
Lukas Ulovec <uloveclukas@gmail.com>
2026-01-14 12:20:40 +0100

    Add aspire docker publish

--- ServiceDesk/ServiceDesk.AppHost/AppHost.cs
+++ ServiceDesk/ServiceDesk.AppHost/AppHost.cs
@@ -1,5 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddDockerComposeEnvironment("env");
+
 var sql = builder.AddSqlServer("sql")
     .WithLifetime(ContainerLifetime.Persistent)
     .WithDataVolume()
