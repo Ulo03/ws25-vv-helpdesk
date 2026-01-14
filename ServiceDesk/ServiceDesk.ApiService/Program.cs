f174cc57187ea2c0e17bbfcfd9c7216cabad529d
Raphael Rumpler <200441@fhwn.ac.at>
2026-01-14 17:07:22 +0100

    added auto migrations for database

--- ServiceDesk/ServiceDesk.ApiService/Program.cs
+++ ServiceDesk/ServiceDesk.ApiService/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
 using ServiceDesk.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,12 @@ builder.AddServiceDefaults();
 // Add SQLServer DbContext
 builder.AddSqlServerDbContext<ServiceDeskDbContext>("servicedesk");
 var app = builder.Build();
+
+await using (var scope = app.Services.CreateAsyncScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ServiceDeskDbContext>();
+    await db.Database.MigrateAsync();
+}
 
 app.MapDefaultEndpoints();
 
---
+++ ServiceDesk/ServiceDesk.Data/ServiceDeskDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ServiceDesk.Data;
+
+public class ServiceDeskDbContextFactory
+    : IDesignTimeDbContextFactory<ServiceDeskDbContext>
+{
+    public ServiceDeskDbContext CreateDbContext(string[] args)
+    {
+        var options = new DbContextOptionsBuilder<ServiceDeskDbContext>()
+            .UseSqlServer(
+                "Server=localhost;Database=servicedesk;Trusted_Connection=True;TrustServerCertificate=True")
+            .Options;
+
+        return new ServiceDeskDbContext(options);
+    }
+}
